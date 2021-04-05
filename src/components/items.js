d3bcb6671cf24b231430238743d9b0c027e4fe1b
Bujala sarath kumar reddy <75170692+Sarath9380556175@users.noreply.github.com>
2021-04-05 16:50:14 +0530

    Update items.js

--- src/components/items.js
+++ src/components/items.js
@@ -41,7 +41,7 @@ class Items extends React.Component{
         console.log(price)
 
         axios({
-            url:'https://dry-citadel-95113.herokuapp.com/itemslist',
+            url:'https://immense-chamber-71560.herokuapp.com/itemslist',
             method:'POST',
             headers:{'content-Type':'application/json'},
             data:
@@ -142,7 +142,7 @@ post = (details) => {
 }
 
 getData = (data) => {
-    return fetch(`https://dry-citadel-95113.herokuapp.com/payment`, {
+    return fetch(`https://immense-chamber-71560.herokuapp.com/payment`, {
         method: "POST",
         headers: {
             Accept: "application/json",
@@ -166,7 +166,7 @@ makePayment = (e) => {
 
     const {name,password,mobilenumber,gender}=this.state;
     axios({
-        url:'https://dry-citadel-95113.herokuapp.com/paymentform',
+        url:'https://immense-chamber-71560.herokuapp.com/paymentform',
         method:'POST',
         headers:{'content-Type':'application/json'},
         data:
