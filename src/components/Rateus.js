97c20f2ef0e439a86676ed8bca77c479646235de
Bujala sarath kumar reddy <75170692+Sarath9380556175@users.noreply.github.com>
2021-04-05 16:41:14 +0530

    Update Rateus.js

--- src/components/Rateus.js
+++ src/components/Rateus.js
@@ -35,7 +35,7 @@ class Rateus extends React.Component{
     handleSubmit=()=>{
         const {rating,quality,feedback}=this.state;
         axios({
-            url:'https://dry-citadel-95113.herokuapp.com/rateus',
+            url:'https://immense-chamber-71560.herokuapp.com/rateus',
             method:'POST',
             headers:{'content-Type':'application/json'},
             data:
