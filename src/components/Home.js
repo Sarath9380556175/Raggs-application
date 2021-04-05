8a7cab44a26d3c1f8642901e1635927db2316d70
Bujala sarath kumar reddy <75170692+Sarath9380556175@users.noreply.github.com>
2021-04-05 17:13:02 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,7 +16,7 @@ class Home extends React.Component{
  {
    sessionStorage.clear();
    axios({
-     url:'https://immense-chamber-71560.herokuapp.com/locations',
+     url:'https://vast-plateau-26931.herokuapp.com/locations',
      method:'GET',
      headers:{'content-Type':'application/json'}
    })
@@ -26,7 +26,7 @@ class Home extends React.Component{
 
 
    axios({
-     url:'https://immense-chamber-71560.herokuapp.com/quicksearches',
+     url:'https://vast-plateau-26931.herokuapp.com/quicksearches',
      method:'GET',
      headers:{'content-Type':'application/json'}
    }).then(res=>this.setState({quicksearches:res.data.quicksearch}))
